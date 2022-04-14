9f23e8760ecad8533c3701bcef6a3f9d1ae5d55f
Từ Thắng Phát <81029660+phatdev12@users.noreply.github.com>
2022-04-14 20:26:22 +0700

    Update say.js

--- say.js
+++ say.js
@@ -1,5 +1,6 @@
 const text = args.slice().join(" ");
+if(!text) return message.reply("Please enter something to say!")
 if(text.includes("@everyone" || "@here")){
-  return false;
+  return message.reply("Please don't ping everyone");
 }
 message.reply(`Say: ${text}`);
